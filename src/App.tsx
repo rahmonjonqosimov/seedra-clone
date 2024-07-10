235f269471d127f630de1b1e1d7b7b390f41e83d
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2024-07-10 11:59:48 +0500

    the end

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import HomePage from "./pages/home";
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
 import Detail from "./pages/detail";
 import Wishlist from "./pages/wishlist";
 import Products from "./pages/products";
